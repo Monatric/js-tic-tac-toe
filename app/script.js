f7d3a0adb4ab8048a893b75d2a51835d33da96eb
Monatric <nawpnawp8@gmail.com>
2025-08-21 20:25:33 +0800

    feat: add cell and gameboard objects

--- app/script.js
+++ app/script.js
@@ -1,1 +1,22 @@
-console.log("connected")
+const gameboard = (function () {
+  const board = []
+  for (let i = 0; i < 9; i++) {
+    board[i] = cell()
+  }
+
+  const printBoard = () => {
+    console.log(`
+     ${board[0].mark} | ${board[1].mark} | ${board[2].mark}  
+    ---|---|---
+     ${board[3].mark} | ${board[4].mark} | ${board[5].mark}
+    ---|---|---
+     ${board[6].mark} | ${board[7].mark} | ${board[8].mark}  
+  `)
+  }
+
+  return { printBoard }
+})();
+
+function cell(mark = ' ') {
+  return { mark }
+}; 
